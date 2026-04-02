530bb47f6312984406b846de4961cad0d579f397
Sergio75 <sergio@kframeinteractive.com>
2026-04-02 12:11:39 +0200

    release: v0.9.4-alpha

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ echo "   / /_\  / /__ / /___/   V /  / /\/ /"
 echo "  / /   _/ /__ / /___/ /V \ \ / / / /"
 echo " /_/   /______/______/_/ \_\ \/  /_/"
 echo ""
-echo "  From Vision to Form              v0.9.3-alpha
+echo "  From Vision to Form              v0.9.4-alpha
 echo ""
 echo "  +--------------------------------------+"
 echo "  :                                      :"
